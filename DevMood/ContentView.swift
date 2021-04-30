8ef4567338578fd26178450dbfb812dda74d9192
yerinaoh <orina8262@gmail.com>
2021-04-30 23:57:19 +0900

    fix_viewmodel

--- DevMood/ContentView.swift
+++ DevMood/ContentView.swift
@@ -10,34 +10,22 @@ import CoreData
 import RxSwift
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
     var title: String = "null"
 
     var body: some View {
         
         Text(title)
         Button("GOOD") {
-            
         }
         Button("GOOD") {
-            
         }
         Button("GOOD") {
-            
         }
     }
-    
-    var viewModel: AnnounceViewModel! {
-        didSet {
-          self.viewModel.moodDidChange = { vm in
-            self.title = vm.announce ?? ""
-          }
-        }
-      }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: AnnounceViewModel(mood: Mood(count: 10)))
+        ContentView(title: "title")
     }
 }
